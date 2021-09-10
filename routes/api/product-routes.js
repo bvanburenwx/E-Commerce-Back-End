f87d9c4def0895ceeacb580a7f9c95daf9cf15ac
BVanBuren <bvanburenwx@gmail.com>
2021-09-09 20:00:16 -0400

    finished product-routes.js. Added the get all products and get by primarykey routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,17 +1,39 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+const { findByPk } = require('../../models/Category');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+  res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if(!productData) {
+      res.status(404).json({ message: 'Product not found with given ID'});
+      return;
+    }
+    res. status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
